e2b55cf21dd83ad411ec18ee0488f1a3281123d6
TJ Biddle <tj@inflection.com>
2012-12-06 20:51:54 -0800

    updating deployment script to account for when the directory may be empty to still clone the repo

--- puppet/thomasbiddle_info/files/update_thomasbiddle_info.sh
+++ puppet/thomasbiddle_info/files/update_thomasbiddle_info.sh
@@ -5,6 +5,9 @@ GIT_REPO="https://github.com/thomasbiddle/ThomasBiddle.info.git"
 if [[ ! -d "${SITE_BASE}/${SITE_DIRECTORY}" ]]
 then
 	cd ${SITE_BASE} && git clone ${GIT_REPO}
+elif [[ ! "$(ls -A ${SITE_BASE}/${SITE_DIRECTORY})" ]]
+then
+    cd ${SITE_BASE} && git clone ${GIT_REPO}
 else
 	cd ${SITE_BASE}/${SITE_DIRECTORY} && git pull
 fi
