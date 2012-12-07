b004e96bf1b26290b105aa1b2626be4bc0dfb6e6
TJ Biddle <tj@inflection.com>
2012-12-06 19:50:12 -0800

    Updated deployment script

--- puppet/thomasbiddle_info/files/update_thomasbiddle_info.sh
+++ puppet/thomasbiddle_info/files/update_thomasbiddle_info.sh
@@ -1,1 +1,10 @@
-cd $HOME/Sites/ThomasBiddle.info; git pull
+SITE_BASE="/srv/www"
+SITE_DIRECTORY="ThomasBiddle.info"
+GIT_REPO="https://github.com/thomasbiddle/ThomasBiddle.info.git"
+
+if [[ ! -d "${SITE_BASE}/${SITE_DIRECTORY}" ]]
+then
+	cd ${SITE_BASE} && git clone ${GIT_REPO}
+else
+	cd ${SITE_BASE}/${SITE_DIRECTORY} && git pull
+fi
